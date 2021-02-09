164bd68a562a2e93aed1b339aa093a769364fb52
Mac-lab profile <Mac@444.local>
2021-02-09 20:13:28 +0100

    commit 2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
+import { using } from 'rxjs';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
   }
+
+  //Http Get route named hello2
+  @Get('hello2')
+  @Render('index.html')
+  gethello2():{} {
+    return this.appService.gethello2();
+  }
+
+  //Http Get route named Home
+  @Get('Home')
+  @Render('home.html')
+  getHome(): {} {
+    return this.appService.getHome();
+  }
+
+  //Http Get route name about-us
+  @Get('about-us')
+  @Render('about-us.html')
+  getAboutUs() : {} {
+    return this.appService.getAboutUs();
+  }
 }
+
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
+//Let's make it a NestExpressApplication
+import {NestExpressApplication} from '@nestjs/platform-express';
+
+//We need JOIN to synthesize the directory path which will contain templates
+import {join} from 'path';
+
+//We need nunjucks as render engine
+import * as nunjucks from 'nunjucks';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule,);
+
+  //To associate nunjucks with express, we need to get the underlying express platform from nest
+  const express = app.getHttpAdapter().getInstance();
+
+  //we also need to get the directory name views (create it in project root directory), 
+  //which is the root directory for our template files
+  const views = join(__dirname, '..', 'views');
+
+  //configure nunjucks, setting views and express declared above
+  nunjucks.configure(views, {express});
+
+  //we need to get directory name static
+  const staticAssets = join(__dirname, '..', 'static');
+  app.useStaticAssets(staticAssets);
+
+  //start the application
+  await app.listen(5000);
 }
 bootstrap();
