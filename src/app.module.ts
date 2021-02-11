aa39aa41eb1338818694131a183d422429fbe997
Mac-lab profile <Mac@444.local>
2021-02-11 15:05:13 +0100

    11/02/2021

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StudentRegisterationModule } from './student-registeration/student-registeration.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(), StudentRegisterationModule],
   controllers: [AppController],
   providers: [AppService],
 })
